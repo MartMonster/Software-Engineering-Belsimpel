bcd6264a2b93960547386406384e03ddf30476b0
EraXon <bbruma51@yahoo.com>
2023-03-14 21:59:00 +0100

    Merged some migrations

--- foosball-ranking/database/migrations/2014_10_12_000000_create_users_table.php
+++ foosball-ranking/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('username')->unique();
+            $table->string('lastname');
+            $table->boolean('isAdmin')->default(FALSE);
         });
     }
 
