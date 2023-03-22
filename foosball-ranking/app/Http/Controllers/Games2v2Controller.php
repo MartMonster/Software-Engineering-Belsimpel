ccbb18f7ae613cffce935d5a4d8b505dbc7b151e
EraXon <bbruma51@yahoo.com>
2023-03-22 18:58:31 +0100

    Added 2v2 last 10 games

--- foosball-ranking/app/Http/Controllers/Games2v2Controller.php
+++ foosball-ranking/app/Http/Controllers/Games2v2Controller.php
@@ -18,10 +18,29 @@ class Games2v2Controller extends Controller
                         ->orOn('t2.player2_id', '=', 'p.id');
                 })
                 ->where('p.id', '=', Auth::id())
-                ->select('g.*','t1.*','t2.*','t1.team_name AS t1_name')
-                ->get();
+                ->select('g.id',
+                'g.team1_score',
+                'g.team2_score',
+                'g.created_at',
+                'g.updated_at',
+                't1.team_name AS t1_name',
+                't2.team_name AS t2_name'
+                )->paginate(10);
         return($result);
-
+    }
+    public function getLast10Games(){
+        $result = DB::table('games2v2 as g')
+                ->join('foosball_teams as t1', 'g.team1_id', '=', 't1.id')
+                ->join('foosball_teams as t2', 'g.team2_id', '=', 't2.id')
+                ->select('g.id',
+                'g.team1_score',
+                'g.team2_score',
+                'g.created_at',
+                'g.updated_at',
+                't1.team_name AS t1_name',
+                't2.team_name AS t2_name'
+                )->paginate(10);
+        return $result;
     }
 
 }
