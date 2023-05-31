ddd1acb40c2d47ab6d787ff01581ce881d754f3e
EraXon <bbruma51@yahoo.com>
2023-05-31 16:35:11 +0200

    Deleted the email verfication+delete2v2game tests

--- foosball-ranking/app/Http/Controllers/Auth/VerifyEmailController.php
+++ foosball-ranking/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -13,7 +13,7 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+   /* public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended(
@@ -28,5 +28,5 @@ class VerifyEmailController extends Controller
         return redirect()->intended(
             config('app.frontend_url') . RouteServiceProvider::HOME . '?verified=1'
         );
-    }
+    }*/
 }
