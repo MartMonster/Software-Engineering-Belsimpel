1dd4617a605cae9648335e63f2037cce68f16b3b
MartMonster <martmonster1@gmail.com>
2023-06-07 21:30:28 +0200

    fix for unique email constraint not being unique enough

--- foosball-ranking/database/factories/UserFactory.php
+++ foosball-ranking/database/factories/UserFactory.php
@@ -18,11 +18,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $username = fake()->unique()->userName();
         return [
             'name' => fake()->name(),
-            'username' => fake()->unique()->userName(),
+            'username' => $username,
             'lastname' => fake()->lastName(),
-            'email' => preg_replace('/@example\..*/', '@belsimpel.nl', fake()->unique()->safeEmail),
+            'email' => $username . '@belsimpel.nl',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
